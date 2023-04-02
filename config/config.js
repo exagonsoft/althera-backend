36b5c65473fee11020ddc23fb1d61dfd99218524
melkor@aleph.engineering <melkor@aleph.engineering>
2023-04-01 22:35:24 -0400

    :new: ***Set up Environment***

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    host: process.env.HOST || "",
+    port: process.env.PORT || "",
+    database: process.env.DATABASE || "",
+    admin: process.env.ADMIN || "",
+    password: process.env.PASSWORD || "",
+    support_email: process.env.SUPPORT_EMAIL || ""
+}
